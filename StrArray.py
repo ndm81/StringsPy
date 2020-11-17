1bdb5f59a13d357dc0a276d81beaf434114fddad
Nadeem012 <55021916+ndm81@users.noreply.github.com>
2020-11-17 18:29:37 +0530

    Add files via upload

---
+++ StrArray.py
@@ -0,0 +1,20 @@
+# String as Array
+#x="What happend";
+#print(x[11]);
+
+# Slicing
+a="today is cold";
+print(a[2:6]);  # String as Array
+
+b="Hey what happend"
+print(b[-3:-6])
+
+a="Hello World";
+print(len(a))    # Determine Length of String
+
+a="Hello World";
+print(a.upper())  # Convert into Upper Case
+print(a.lower())  # Convert into Lower case
+
+x="Animal beast";
+print(x.replace("A","E")); # Replacing Elements
